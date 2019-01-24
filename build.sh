b60638e3d4c8d0d84d6cd99d77e754497acb8cae
jungju <leejungju.go@gmail.com>
2019-01-25 00:15:45 +0900

    update box

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 ver=`cat VERSION`
-packer build -var 'version=$ver' ubuntu1804.json
+token=`cat ~/tokens/vagrant_cloud_token`
+
+packer build -var 'version='$ver'' -var 'vagrant_cloud_token='$token'' ubuntu1804.json
